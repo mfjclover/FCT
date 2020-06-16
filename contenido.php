66234cfa2ca1bc6edee9a66180bff60b69bef22f
Marcos Fu Jin <mfjclover@gmail.com>
2020-06-16 22:16:33 +0200

    Listar contenido del directorio

--- contenido.php
+++ contenido.php
@@ -2,13 +2,34 @@
 session_start();
 if (isset($_SESSION['s_usuario'])){
     $s_usuario=$_SESSION["s_usuario"];
-    echo "Hola $s_usuario" . "<br>";
+    echo "Hola $s_usuario" . "<br> <br>";
 }
+$directorio_usuario = opendir("./directorio_fichero/" . $s_usuario);
+echo "<table>";
+    echo "<tr>";
+        echo "<th>Nombre</th>";
+        echo "<th>Ver</th>";
+        echo "<th>Editar</th>";
+        echo "<th>Borrar</th>";
+    echo "</tr>";
+    while(($nombreFichero = readdir($directorio_usuario)) != FALSE)
+    {
+        if($nombreFichero != "." && $nombreFichero != "..")
+        {           
+            echo "<tr>";
+            echo "<td>$nombreFichero</td>";
+            echo "<th>Ver</th>";
+            echo "<th>Editar</th>";
+            echo "<th>Borrar</th>";
+            echo "</tr>";
+        }
+    }
+echo "</table>";
 ?> 
 <!DOCTYPE html>
 <html>
 <body>
-	<p>Subir fichero</p> <br>
+	<p>Subir fichero</p>
     <form action="subir_fichero.php" method="post" enctype="multipart/form-data">
         Selecciona un fichero a subir:
         <input type="file" name="fichero_a_subir"> <br>
--- subir_fichero.php
+++ subir_fichero.php
@@ -1,10 +1,21 @@
 <?php
-if(is_uploaded_file($_FILES["fichero_a_subir"]["tmp_name"]))
-{
-    $nombre_original = $_FILES["fichero_a_subir"]["name"];
-    copy($_FILES["fichero_a_subir"]["tmp_name"], "./directorio_fichero/$nombre_original");
+session_start();
+if (isset($_SESSION['s_usuario'])){
+    $s_usuario=$_SESSION["s_usuario"];    
+    if(is_uploaded_file($_FILES["fichero_a_subir"]["tmp_name"]))
+    {
+        $nombre_original = $_FILES["fichero_a_subir"]["name"];
+        copy($_FILES["fichero_a_subir"]["tmp_name"], "./directorio_fichero/$s_usuario/$nombre_original");
+        echo "Se ha subido correctamente el fichero: $nombre_original" . "<br>";
+        echo "<a href='contenido.php'>Volver</a>";
+    }
+    else {
+        echo "Error: No se ha subido el fichero correspondiente";
+    }
 }
+
 else{
-    echo "Error";
+    echo "Error: Acceso denegado";
+    echo "<a href='index.php'>Volver</a>";
 }
 ?>
